53de7146cd9dec44f15b423c1bae4d5b62b9c3eb
Buğra Kasapoğlu <kasapoglub38@gmail.com>
2025-08-06 12:18:34 +0300

    Yükleme ekranı bitirildi.

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:lottie/lottie.dart';
+import 'home_screen.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Lottie.asset(
+          'assets/motions/sound.json',
+          width: 300,
+          height: 300,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
